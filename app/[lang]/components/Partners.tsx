8cf0b4f917798400e4f14b12a9fd44e856f1ba34
abdalraoufalsheikhnajeeb <abdalraoufalsheikhnajeeb@gmail.com>
2024-07-01 21:05:22 +0300

    Update project with latest changes

--- app/[lang]/components/Partners.tsx
+++ app/[lang]/components/Partners.tsx
@@ -75,16 +75,18 @@ const Partners = ({ dic, data, title }: PartnersProps) => {
               key={index}
               className="p-4"
             >
-              <div className="bg-gray-200 border rounded-lg shadow transition-transform duration-300 hover:scale-105 hover:shadow-lg h-full flex flex-col justify-center items-center">
-                <Image
-                  loading="lazy"
-                  quality={20}
-                  width={400}
-                  height={200}
-                  className="rounded-t-lg aspect-video object-cover"
-                  src={card.src}
-                  alt={title}
-                />
+              <div className="bg-gray-200 border rounded-lg shadow transition-transform duration-300 hover:scale-105 hover:shadow-lg h-full flex flex-col justify-center items-center overflow-hidden">
+                <div className="bg-white w-full">
+                  <Image
+                    loading="lazy"
+                    quality={20}
+                    width={400}
+                    height={200}
+                    className="rounded-t-lg aspect-video object-cover"
+                    src={card.src}
+                    alt={title}
+                  />
+                </div>
                 <div className="p-5 flex-grow flex flex-col justify-between">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">
                     {title}
